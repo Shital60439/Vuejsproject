ad502201b3fb08097b3f07b3ee17bc7046f3e298
Shital Ankolikar <shital.ankolikar@olacabs.com>
2022-07-20 18:04:57 +0530

    props passed child to parent

---
+++ vue1/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="app">
+  <h1>{{title}}</h1>
+  <FirstFile v-on:changeTitle="updateTitleText($event)"></FirstFile>
+  </div>
+  </template>
+<script>
+import FirstFile from './components/first-file.vue'
+export default {
+  name: 'App',
+  components: {
+    FirstFile
+    },
+    data(){
+    return{
+      title:" child to parent"
+    }
+  },
+  methods:{
+    updateTitleText(title)
+    {
+      this.title=title
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue1/src/components/first-file.vue
@@ -0,0 +1,30 @@
+
+<template>
+<div>
+    <!-- <h1 style="white-space:pre-line; color: aqua;">{{message}}</h1>
+    <textarea type="text" v-model="message"/> -->
+    <button v-on:click="updateTitle">Update Title</button>
+
+</div>
+</template>
+<script>
+export default{
+    name:"first-file",
+    methods:{
+        updateTitle(){
+            this.$emit('changeTitle',' changed...  props has been passed')
+        }
+
+    }
+     
+};
+</script>
+<!-- <style>
+.vueStyle{
+    color:orange
+}
+.display{
+    background-color: beige;
+    padding: 5px;
+}
+</style> -->
